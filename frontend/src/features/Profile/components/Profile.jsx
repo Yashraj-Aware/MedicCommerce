c70c1b07b7bed084069d570605e014bf2bfdbd6d
Yashraj Aware <yashrajaware2003@gmail.com>
2024-02-07 23:37:37 +0530

    components of account

--- frontend/src/features/Profile/components/Profile.jsx
+++ frontend/src/features/Profile/components/Profile.jsx
@@ -1,53 +1,36 @@
-import React from 'react'
-import styles from '../profile.module.css'
+import React from "react";
+import styles from "../profile.module.css";
 
 const Profile = () => {
-
-    const OrderList = () => {
-
-
-    }
+  
   return (
     <main className="mx-auto max-w-7xl ">
-<div className="flex items-baseline justify-between border-b border-gray-200 pb-12 pt-24 bg-[#c7dde8]">
-            <h1 className="sm:text-4xl lg:text-4xl ml-7 mb-7 font-bold tracking-tight text-gray-900 ">Orders</h1>
+      <div className="flex items-baseline justify-between border-b border-gray-200 pb-12 pt-24 bg-[#c7dde8]">
+        <h1 className="sm:text-4xl lg:text-4xl ml-7 mb-7 font-bold tracking-tight text-gray-900 ">
+          Orders
+        </h1>
       </div>
-      
-    <div className={styles.container}>
-        {/* <div className={styles.leftContainer}>
-            <div className="userDashboard">
-                <button>Dashboard</button>
-            </div>
-            <div className="userOrders">
-            <button>Orders</button>
-            </div>
-            <div className="userAddress">
-            <button>Address</button>
-            </div>
-        </div> */}
-        <div className={styles.rightContainer}>
-        <div className={styles.titles}>
-            <h3 className={styles.order}>Order</h3>
-            <h3 className={styles.date}>Date</h3>
-            <h3 className={styles.status}>Status</h3>
-            <h3 className={styles.total}>Total</h3>
-            {/* <h3 className={styles.action}></h3> */}
-</div>
-            <div className={styles.OrderItem}>
-            <div className={styles.OrderList}>
-        <h3>1</h3>
-        <h3>Item</h3>
-        <h3>date</h3>
-        <h3>date</h3>
-        {/* <h3>date</h3> */}
-    </div>
-            </div>
+
+      <div className="parent mt-10 mx-4 h-96 grid grid-cols-[2fr_4fr]">
+
+        <div className="left-container">
+          <div className="elements flex flex-col justify-center text-center py-7 ">
+            <a href="#dash" className="">Dashboard</a>
+            <a href="#orders" className="">Orders</a>
+            <a href="#address" className="">Address</a>
+            <a href="#acc" className="">Account Details</a>
+            <a href="#logout" className="">Logout</a>
           </div>
         </div>
-      
-  
-    </main>
-  )
-}
 
-export default Profile
+        <div className="right-container">
+
+        </div>
+
+
+      </div>
+    </main>
+  );
+};
+
+export default Profile;
--- frontend/src/features/navbar/Navbar.jsx
+++ frontend/src/features/navbar/Navbar.jsx
@@ -147,7 +147,7 @@ export default function Navbar() {
                             href="/profile"
                             className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                           >
-                            My Orders
+                            My Account
                           </a>
                         )}
                       </Menu.Item>
