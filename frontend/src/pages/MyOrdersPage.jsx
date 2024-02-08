dfcc6b6e67a321fc73b7699509c3fd95bc85e1c4
Yashraj Aware <yashrajaware2003@gmail.com>
2024-02-08 09:13:24 +0530

    Page creation and routing

---
+++ frontend/src/pages/MyOrdersPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../features/navbar/Navbar";
+import MyOrders from "../features/Profile/components/MyOrders";
+const MyOrdersPage = () => {
+  return (
+    <div>
+      <Navbar />
+      <MyOrders />
+    </div>
+  );
+};
+
+export default MyOrdersPage;
